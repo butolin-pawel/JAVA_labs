c281b2f5f6e32e84e169cb866714886a1e737e3b
butolin-pawel <99872063+butolin-pawel@users.noreply.github.com>
2022-12-31 13:47:59 +0300

    Delete task14 directory

--- task14/src/main/java/com/vyatsu/task14/configuration/SecurityWebApplicationInitializer.java
+++
@@ -1,9 +0,0 @@
-package com.vyatsu.task14.configuration;
-
-// import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-//import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-import org.springframework.stereotype.Component;
-
-@Component
-public class SecurityWebApplicationInitializer {//extends AbstractSecurityWebApplicationInitializer {
-}
